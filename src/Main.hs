87e9ec239cad1fc300230965fe81608cb5c9a90b
Ruud van Asseldonk <dev@veniogames.com>
2016-08-26 20:28:12 +0200

    Initial commit with minimal Stack project

---
+++ src/Main.hs
@@ -0,0 +1,9 @@
+-- Earl -- A flexible redirect server
+-- Copyright 2016 Ruud van Asseldonk
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- A copy of the License has been included in the root of the repository.
+
+main :: IO ()
+main = putStrLn "Hello, world"
